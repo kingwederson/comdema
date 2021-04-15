75aa379c8835c075898c0f2dae6f8f7bbf81e1d9
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2021-04-15 12:33:35 -0300

    Ignition

---
+++ cabecalho.js
@@ -0,0 +1,22 @@
+
+function cabi(){
+    var cabecalho = window.document.getElementById('cabecalho')
+    var htmlcab = `
+    <nav class="main" id="cab">
+        <img src="https://i.imgur.com/KIqa8eh.png" class="logo">
+        <nav class="titulo"><span id="hie1">COMDEMA</span><br><span id="hie2">Bom Bardim de Minas</span></nav>
+        <nav id="menu">
+            <ul class="menu">
+                <li>Regimento Interno</li>
+                <a href="#leis"><li>Leis</li></a>
+                <li>Portarias</li>
+                <li>Deliberações</li>
+                <li>Atas</li>
+                <li>Relatórios</li>
+            </ul>
+        </nav>
+    </nav>
+    `;
+   cabecalho.innerHTML = htmlcab
+}
+cabi()
