115da40ce79a9e950db5bbec758a97606f820158
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2021-04-16 00:17:28 -0300

    Estilização.

--- rodape.js
+++ rodape.js
@@ -3,8 +3,8 @@ function rodapi(){
     var rodape = window.document.getElementById('rodape')
     var htmlrodape = `
     <nav class="main" id="rodape">
-        <p>Disclamer: Este site é apenas um repositório de informações úteis para o bom funcionamento do COMDEMA e para dar publicidade às atividades realizadas.</p>
-        <p>COMDEMA, 2021.</p>
+        <p class="lista">Disclamer: Este site é apenas um repositório de informações úteis para o bom funcionamento do COMDEMA e para dar publicidade às atividades realizadas.</p>
+        <p class="lista"COMDEMA, 2021.</p>
     </nav>
     `;
    rodape.innerHTML = htmlrodape
