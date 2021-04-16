6f85ca21debad0037c2d684d8d0258b22660ac05
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2021-04-16 13:40:02 -0300

    Add files via upload

--- cabecalho.js
+++ cabecalho.js
@@ -9,14 +9,14 @@ function cabi(){
         </a>
         <nav id="menu">
             <ul class="menu">
-                <a href="regimento/"><li>Regimento Interno</li></a>
-                <a href="leis/"><li>Leis</li></a></a>
-                <a href="portarias/"><li>Portarias</li></a>
-                <a href="deliberacoes/"><li>Deliberações</li></a>
-                <a href="atas/"><li>Atas</li></a>
-                <a href="oficios/"><li>Ofícios</li></a>
-                <a href="relatorios/"><li>Relatórios</li></a>
-                <a href="noticias/"><li>Notícias</li></a>
+                <a href="http://comdema.wederson.net.br/regimento/"><li>Regimento Interno</li></a>
+                <a href="http://comdema.wederson.net.br/leis/"><li>Leis</li></a></a>
+                <a href="http://comdema.wederson.net.br/portarias/"><li>Portarias</li></a>
+                <a href="http://comdema.wederson.net.br/deliberacoes/"><li>Deliberações</li></a>
+                <a href="http://comdema.wederson.net.br/atas/"><li>Atas</li></a>
+                <a href="http://comdema.wederson.net.br/oficios/"><li>Ofícios</li></a>
+                <a href="http://comdema.wederson.net.br/relatorios/"><li>Relatórios</li></a>
+                <a href="http://comdema.wederson.net.br/noticias/"><li>Notícias</li></a>
             </ul>
         </nav>
     </nav>
