bb63624183c32f00b6dc24b130a8b7c18ba2fe6c
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2021-04-16 15:54:51 -0300

    Add files via upload

--- lateral.js
+++ lateral.js
@@ -1,7 +1,7 @@
 function laterali(){
     var lateral = window.document.getElementById('secundaria')
     var htmllateral = `
-    <nav id="ligacoes">
+    <nav id="ligacoes"><input type="button" classe="apagar" value="X" onclick="apagar()"><br>
             <ul class="lateral"><h2>Links Institucionais</h2>
                 <a href="https://www.cmbj.mg.gov.br/"><li>Poder Legislativo</li></a>
                 <a href="https://www.bomjardimdeminas.mg.gov.br/"><li>Poder Executivo</li></a>
@@ -14,3 +14,6 @@ function laterali(){
    lateral.innerHTML = htmllateral
 }
 laterali()
+function apagar(){
+    window.document.getElementById('ligacoes').style.display = "none"
+}
